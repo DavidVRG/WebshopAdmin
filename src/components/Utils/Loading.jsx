564f7f5b06df19c27caaad88118f61036de661e5
Varga Dávid <vargadavid946@gmail.com>
2023-04-27 15:40:55 +0200

    Upload Source Code

--- src/components/Utils/Loading.jsx
+++ src/components/Utils/Loading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Loading() {
     return (
         <div className='fixed top-0 left-0 right-0 bottom-0 h-screen w-screen flex justify-center items-center'>
-            <img src="/Loading.gif" alt="Loading" />
+            <img src="Loading.gif" alt="Loading" />
         </div>
     )
 }
